02a6770456fd5321ecfb4590eb7e4708c5cb2fd4
welldyrosman <82356487+welldyrosman@users.noreply.github.com>
2022-11-05 09:02:59 +0700

    dasdas

---
+++ assets/pinia-legacy.4c37ab54.js
@@ -0,0 +1,7 @@
+System.register(["./@vue-legacy.fac961d2.js"],(function(t,e){"use strict";var c,n,r;return{setters:[t=>{c=t.e,n=t.r,r=t.m}],execute:function(){t("c",(function(){const t=c(!0),s=t.run((()=>n({})));let i=[],u=[];const a=r({install(t){a._a=t,t.provide(e,a),t.config.globalProperties.$pinia=a,u.forEach((t=>i.push(t))),u=[]},use(t){return this._a?i.push(t):u.push(t),this},_p:i,_a:null,_e:t,_s:new Map,state:s});return a}));
+/*!
+        * pinia v2.0.23
+        * (c) 2022 Eduardo San Martin Morote
+        * @license MIT
+        */
+const e=Symbol();var s;!function(t){t.direct="direct",t.patchObject="patch object",t.patchFunction="patch function"}(s||(s={}))}}}));
